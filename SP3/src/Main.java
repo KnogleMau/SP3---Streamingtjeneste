f2d3b0c838b0f9e02371c34258856a817faf5560
KnogleMau <mav.solver@hotmail.com>
2024-11-22 12:27:36 +0100

    Load i StartMenu

--- SP3/src/Main.java
+++ SP3/src/Main.java
@@ -2,5 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        StartMenu startMenu = new StartMenu();
+
+        startMenu.load();
     }
 }
--- SP3/src/StartMenu.java
+++ SP3/src/StartMenu.java
@@ -1,2 +1,32 @@
 public class StartMenu {
+    private String username;
+    private String password;
+    FileIO fileIO = new FileIO();
+    TextUI textUI = new TextUI();
+
+    public void load(){
+     boolean input =  textUI.promptBinary("Vil du gerne oprette en bruger " + "Y/N!");
+        if(input == true){
+            createUser();
+        } else{
+            boolean input1 = textUI.promptBinary("Vil du istedet Log in? " + "Y/N!");
+            if(input1 == true){
+                loginOption();
+            } else {
+                close();
+            }
+        }
+    }
+    public void loginOption(){
+
+    }
+
+    public void createUser(){
+
+    }
+
+    public void close(){
+
+    }
+
 }
