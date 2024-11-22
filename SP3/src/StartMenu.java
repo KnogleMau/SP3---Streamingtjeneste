fd2ef489f5ead4daf9f2c5850b1e3f31006e4bca
Thranum <thranum2405@gmail.com>
2024-11-22 11:26:04 +0100

    A+A-StartMenu

--- SP3/src/StartMenu.java
+++ SP3/src/StartMenu.java
@@ -1,2 +1,21 @@
 public class StartMenu {
+    private String username;
+    private String password;
+
+
+    private void load(){
+
+    }
+    private void loginOption(){
+
+    }
+
+    private void createUser(){
+
+    }
+
+    private void close(){
+
+    }
+
 }
