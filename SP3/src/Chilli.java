148c476d83f4121ebb8bcf2dd29089fb10e82920
KnogleMau <mav.solver@hotmail.com>
2024-11-25 10:37:08 +0100

    Main og chilli

--- SP3/src/Chilli.java
+++ SP3/src/Chilli.java
@@ -5,6 +5,7 @@ public class Chilli {
     public void setup() throws FileNotFoundException {
         StartMenu startMenu = new StartMenu();
         startMenu.load();
+
     }
 
     public void end(){
--- SP3/src/Main.java
+++ SP3/src/Main.java
@@ -3,10 +3,8 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-Chilli chilli = new Chilli();
-
-chilli.setup();
-
-chilli.end();
+        Chilli chilli = new Chilli();
+        chilli.setup();
+        chilli.end();
     }
 }
