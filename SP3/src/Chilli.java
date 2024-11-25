2f9fecf3bd10999ceac3a716f69c7357a43a3ea1
KnogleMau <mav.solver@hotmail.com>
2024-11-25 10:33:33 +0100

    Main og chilli

--- SP3/src/Chilli.java
+++ SP3/src/Chilli.java
@@ -1,2 +1,13 @@
+import java.io.FileNotFoundException;
+
 public class Chilli {
+
+    public void setup() throws FileNotFoundException {
+        StartMenu startMenu = new StartMenu();
+        startMenu.load();
+    }
+
+    public void end(){
+
+    }
 }
--- SP3/src/Main.java
+++ SP3/src/Main.java
@@ -1,12 +1,12 @@
 import java.io.FileNotFoundException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        StartMenu startMenu = new StartMenu();
+Chilli chilli = new Chilli();
 
-        startMenu.load();
+chilli.setup();
 
+chilli.end();
     }
 }
